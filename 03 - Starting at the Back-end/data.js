d740acea604e0b30c1638e3df84d441c331933a4
lemuelZara <lemuel_zara@outlook.com>
2020-06-12 15:45:55 -0300

    class files

--- 03 - Starting at the Back-end/data.js
+++ 03 - Starting at the Back-end/data.js
@@ -20,4 +20,4 @@ module.exports = [
         price: 'Free',
         featured: true,
     }
-]
+] 
--- 03 - Starting at the Back-end/public/scripts.js
+++ 03 - Starting at the Back-end/public/scripts.js
@@ -1,7 +1,7 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 
 const cards = document.querySelectorAll('.card')
- 
+
 for (let card of cards) {
     card.addEventListener('click', function () {
         const id = card.getAttribute('id')
